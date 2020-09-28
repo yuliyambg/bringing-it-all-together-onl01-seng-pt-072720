87c82913e33ba683d3f7178a9858f97f285ca3e5
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 18:07:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,17 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id=nil, name, breed)
-    @id = id
-    @name = name
-    @breed = breed
+  # def initialize(id=nil, name, breed)
+  #   @id = id
+  #   @name = name
+  #   @breed = breed
+  # end
+  
+  def initialize(args)
+    @id = args[0]
+    @name = args[1]
+    @breed = args[2]
   end
+
   
 end
