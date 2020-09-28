ee3a4ae1a01525a0550698669d47f0673fa2320c
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 18:39:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,6 +19,16 @@ class Dog
   h.each {|k,v| public_send("#{k}=",v)}
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
 
+    DB[:conn].execute(sql)
+  end
 
 end
