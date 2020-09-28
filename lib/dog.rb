d2924b2350ceaba605361a38031aeb733bc23ea4
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 19:08:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,6 +40,7 @@ def save
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
+      self
       # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
