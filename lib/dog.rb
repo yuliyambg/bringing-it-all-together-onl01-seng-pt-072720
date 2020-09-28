2393a14f31d7d24dd0f3625765535f14ec6856ca
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 18:31:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,13 +15,10 @@ class Dog
   #   @breed = args[2]
   # end
 
-  # def initialize(h)
-  # h.each {|k,v| public_send("#{k}=",v)}
-  # end
-
-
   def initialize(h)
-  h.each {|k,v| self.k = v }
+  h.each {|k,v| public_send("#{k}=",v)}
   end
+
+
 
 end
