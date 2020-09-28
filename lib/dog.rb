30cfc24dff9b660ece31ebfc8d09892a584ef62c
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 18:49:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,7 +39,31 @@ class Dog
   end
 
   def save
+    if self.id
+       self.update
+     else
+       sql = <<-SQL
+       INSERT INTO dogs (name, breed)
+       VALUES (?, ?)
+       SQL
+       DB[:conn].execute(sql, self.name, self.breed)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+     end
   end
 
+
+# def save
+#     if self.id 
+#       self.update
+#     else
+#       sql = <<-SQL
+#         INSERT INTO dogs (name, breed) 
+#         VALUES (?, ?)
+#       SQL
+
+#       DB[:conn].execute(sql, self.name, self.breed)
+#       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+#     end
+#   end
 
 end
